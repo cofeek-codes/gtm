fc5256d90d537525286fa7dc24b86879d780eaf8
cofeek-codes <kormyshev11@mail.ru>
2023-11-15 11:24:19 +0500

    removed bootstrap; fixed lib

--- main.py
+++ main.py
@@ -1,29 +1,9 @@
 import sys
 import os
 
-from lib.bootstrap import bootstrap, check_bootstrap
-from lib.lib import convert_mp4_to_mp3, download
+from libgtm.lib import convert_mp4_to_mp3, download
 
 def main():
-        
-    # "bootstrap" command
-
-    if (len(sys.argv) == 2) and ("bootstrap" in sys.argv or "-b" in sys.argv):
-        if not check_bootstrap():
-            
-            bootstrap_return_code = bootstrap()
-
-            if bootstrap_return_code != 0:
-                print("error bootstrapping project (downloading binaries)")
-                os._exit(1)
-            else:
-                print("bootstrap successeful")
-                os._exit(0)
-   
-        else:
-            print("bootstrap is already completed")
-            os._exit(0)
-                
     
     # ui
     if "-ui" in sys.argv:
